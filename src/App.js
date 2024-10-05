85e03c8d6708d814461cc2781e1f7b20ca7582a0
ashrafbangash <ashrafbangash088@gmail.com>
2024-10-05 21:52:32 +0500

    Calendar Done
    Calendar Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./App.css";
+import CalendarComp from "./Calendar/Calendar";
+
+function App() {
+  return (
+    <div className="App">
+      <CalendarComp />
+    </div>
+  );
+}
+
+export default App;
